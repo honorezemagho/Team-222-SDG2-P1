2ed2e16e0c57d16afec9e20dff304082796e9219
honorezemagho <honorezemagho@gmail.com>
2020-05-27 16:18:44 +0200

    adding js file for firebase configs

---
+++ src/init.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "Firebase Api Key",
+    authDomain: "Firebase Project Domain",
+    databaseURL: "Firebase database Url",
+    projectId: "Project Id",
+    storageBucket: "Storage Bucket",
+    messagingSenderId: "Message Sender Id",
+    appId: "appId"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
